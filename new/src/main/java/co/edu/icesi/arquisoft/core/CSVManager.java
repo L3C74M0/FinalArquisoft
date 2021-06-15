9fd8b3a9508dc2b8a4764f0c55124f13325be722
R00TKIT <juandavidlectamocaicedo@gmail.com>
2021-06-15 16:01:47 -0500

    CSV class implementedç

--- new/src/main/java/co/edu/icesi/arquisoft/core/CSVManager.java
+++ new/src/main/java/co/edu/icesi/arquisoft/core/CSVManager.java
@@ -5,35 +5,85 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVManager {
-	
+
+	private String configurationName;
+	private long pointsToGenerate;
+	private int seed;
+	private int nodesToUse;
+
 	public CSVManager() {
-		
 	}
-	
-	
-	
-	
-	protected String[] loadCSV() {
-		String[] query = new String[5];
-		
+
+	public String getConfigurationName() {
+		return configurationName;
+	}
+
+	public void setConfigurationName(String configurationName) {
+		this.configurationName = configurationName;
+	}
+
+	public long getPointsToGenerate() {
+		return pointsToGenerate;
+	}
+
+	public void setPointsToGenerate(long pointsToGenerate) {
+		this.pointsToGenerate = pointsToGenerate;
+	}
+
+	public int getSeed() {
+		return seed;
+	}
+
+	public void setSeed(int seed) {
+		this.seed = seed;
+	}
+
+	public int getNodesToUse() {
+		return nodesToUse;
+	}
+
+	public void setNodesToUse(int nodesToUse) {
+		this.nodesToUse = nodesToUse;
+	}
+
+	protected boolean loadCSV() {
+		boolean loaded = false;
+
 		String line = "";
 		String splitBy = ",";
 		try {
-			// parsing a CSV file into BufferedReader class constructor
 			BufferedReader br = new BufferedReader(new FileReader("CSVDemo.csv"));
-			while ((line = br.readLine()) != null) // returns a Boolean value
-			{
-				String[] employee = line.split(splitBy); // use comma as separator
-				System.out.println("Employee [First Name=" + employee[0] + ", Last Name=" + employee[1]
-						+ ", Designation=" + employee[2] + ", Contact=" + employee[3] + ", Salary= " + employee[4]
-						+ ", City= " + employee[5] + "]");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return query;
-		
-	}
+			String[] query = new String[3];
+			while ((line = br.readLine()) != null) {
+				query = line.split(splitBy);
 
+				// The CSV reading order is:
+				/*
+				 * 0. configuration name 1. number of random points to consider 2. seed of the
+				 * random generator to use 3. number of processing nodes to use
+				 */
+
+				// Set configuration name
+				setConfigurationName(query[0]);
+
+				// Set points to generate
+				long points = Long.parseLong(query[1]);
+				setPointsToGenerate(points);
+
+				// Set seed
+				int seedR = Integer.parseInt(query[2]);
+				setSeed(seedR);
+
+				// Set nodes to use
+				int nNodes = Integer.parseInt(query[3]);
+				setNodesToUse(nNodes);
+
+				loaded = true;
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.println("The file could not be loaded");
+		}
+		return loaded;
+	}
 }
